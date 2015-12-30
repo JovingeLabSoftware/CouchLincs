750ff8bc47f63104800ce57b3d84b2811e41627a
Eric J. Kort <ericjkort@gmail.com>
2015-12-30 20:45:53 +0000

    query

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ var assert = require('assert');
 var couchbase = require('couchbase');
 
 describe('LINCS funtion tests', function () {
-    describe('Connectivit.skipy', function () {
-        it.skip('connects to couchbase', function (done) {
+    describe('Connectivity test', function () {
+        it('connects to couchbase', function (done) {
 		    lincs.bucket.get("5", function(err, data) {
 		    	if(err) throw err;
 				assert(data.value.metadata.pert_vehicle === "DMSO");	
@@ -24,6 +24,7 @@ describe('LINCS funtion tests', function () {
     	}
 
         it('retrieves all data for a set of documents', function (done) {
+            this.timeout(5000);
 		    lincs.get(k)
 		    .then(function(data) {
 				assert.equal(data.length, 50);
@@ -36,6 +37,7 @@ describe('LINCS funtion tests', function () {
         });
 
         it('retrieves two metadata fields from a set of documents', function (done) {
+            this.timeout(5000);
 		    lincs.get(k, ["metadata.pert_desc", "metadata.pert_id"])
 		    .then(function(data) {
 				assert.equal(data.length, 50);
@@ -53,7 +55,7 @@ describe('LINCS funtion tests', function () {
         	'CYT001_HA1E_2H_X2_B7_DUO52HI53LO:C18',
         	'CYT001_HT29_2H_X1_B7_DUO52HI53LO:C18', 
         	'CYT001_MCF7_2H_X1_B12:C18']
-		    lincs.getByDistilID(ids, ["metadata.pert_desc", "metadata.pert_dose"])
+        	lincs.instanceQuery({distil_id: ids}, ["metadata.pert_desc", "metadata.pert_dose"])
 		    .then(function(data) {
 				assert.equal(data.length, 4);	
 				done();
@@ -64,10 +66,13 @@ describe('LINCS funtion tests', function () {
         });
 
         it('retrieves all data for all vector controls on a plate', function (done) {
-		    lincs.getByPlate("KDD010_HCC515_96H_X3_B5_DUO52HI53LO", 
-		    			   null, {"pert_type": "ctl_vector"})
+		    lincs.instanceQuery({det_plate: "KDD010_HCC515_96H_X3_B5_DUO52HI53LO",
+		    					 pert_type: "ctl_vector",
+		    					 pert_desc: "lacZ"}, 
+		    					 ["metadata.pert_desc", "metadata.pert_dose"])
 		    .then(function(data) {
-				assert.equal(data[0].metadata.pert_desc, "lacZ");	
+				assert.equal(data[0].pert_desc, "lacZ");	
+				assert.equal(data.length, 2);	
 				done();
 		    })
 			.catch(function(err) {
@@ -87,8 +92,10 @@ describe('LINCS funtion tests', function () {
         });
 
         it('retrieves the second 100 gold instance for cell line A549', function (done) {
-		    lincs.getByCell("A549", 
- 		    			    "metadata", {"is_gold": true}, 100, 100)
+		    lincs.instanceQuery({cell_id: "A549",
+		    					 is_gold: true},
+		    					 ["metadata.pert_desc", "metadata.pert_dose"],
+		    					 100, 100)
 		    .then(function(data) {
 				assert.equal(data.length, 100);	
 				done();
@@ -99,10 +106,14 @@ describe('LINCS funtion tests', function () {
         });
 
         it('retrieves all related instances (same cell line, perturbation, dose, duration)', function (done) {
-	    this.timeout(5000);
-		    lincs.getByPert("BMP7", 'metadata', {pert_dose: 100, pert_time:2}, 0, 5)
-		    .then(function(res) {
-		    	assert.equal(res.length , 5);
+		    this.timeout(5000);
+		    lincs.instanceQuery({cell_id: "A549",
+		    					 pert_desc: "BMP7",
+		    					 pert_dose: 100,
+		    					 pert_time: 2 },
+		    					 ["metadata.pert_desc", "metadata.pert_dose"])
+		    .then(function(data) {
+		    	assert.equal(data.length , 3);
 				done();
 		    })
 		    .catch(function(e) {
@@ -111,12 +122,16 @@ describe('LINCS funtion tests', function () {
         });
 
 	  it('retrieves all vehicle instances from the same plate for a given instance', function(done) {
-	    this.timeout(5000);
+		    this.timeout(5000);
 	    	lincs.get("5", "metadata.det_plate")
 	    	.then(function(res) {
-	    		return(lincs.getByPlate(res[0].det_plate, 
-	    		       null, {"pert_type": "ctl_vehicle"}))})
-		    .then(function(res) {
+	    		var p =  lincs.instanceQuery({det_plate: res[0].det_plate, 
+	    									  pert_type: "ctl_vehicle"},
+			  	    					     ["metadata.pert_desc", 
+			  	    					      "metadata.pert_dose"]);
+  			    return(p);
+	    	})
+			.then(function(res) {
 		    	assert.equal(res.length , 13);
 				done();
 		    })
