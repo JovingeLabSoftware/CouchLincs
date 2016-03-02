c0c74a0be9b8b39edb2f9d3a22d4306a6a2101ed
Eric Kort <eric.kort@vai.org>
2016-03-02 21:34:23 +0000

    minor bug fixes

--- test/test.js
+++ test/test.js
@@ -23,6 +23,18 @@ describe('LINCS funtion tests', function () {
     		c++;
     	}
 
+        it('retrieves all data for a set of documents by view query', function (done) {
+            this.timeout(5000);
+		    lincs.zsvc(["CPC005_A375_6H_X1_B3_DUO52HI53LO:K06","CPC005_A375_6H_X2_B3_DUO52HI53LO:K06","CPC005_A375_6H_X3_B3_DUO52HI53LO:K06","CPC005_A375_6H_X1_B3_DUO52HI53LO:C19","CPC005_A375_6H_X2_B3_DUO52HI53LO:C19","CPC005_A375_6H_X3_B3_DUO52HI53LO:C19","CPC004_A375_6H_X1_B3_DUO52HI53LO:K13","CPC004_A375_6H_X2_B3_DUO52HI53LO:K13","CPC004_A375_6H_X3_B3_DUO52HI53LO:K13","CPC005_A375_6H_X1_B3_DUO52HI53LO:K20","CPC005_A375_6H_X2_B3_DUO52HI53LO:K20","CPC005_A375_6H_X3_B3_DUO52HI53LO:K20","CPC005_A375_6H_X1_B3_DUO52HI53LO:F19","CPC005_A375_6H_X2_B3_DUO52HI53LO:F19"])
+		    .then(function(data) {
+				assert.equal(data.length, 14);
+				done();
+		    })
+			.catch(function(err) {
+				done(err);
+			});	
+        });
+
         it('retrieves all data for a set of documents', function (done) {
             this.timeout(5000);
 		    lincs.get(k)
